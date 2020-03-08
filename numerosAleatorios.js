265cffdf6ab0adff4fc4573108dd386afba35b67
Marcos M. Crocci <marcoscrocci@gmail.com>
2020-03-08 00:11:29 -0300

    numerosAleatorios.js

---
+++ numerosAleatorios.js
@@ -0,0 +1,9 @@
+// Gerar um código de 6 digitos aleatórios
+const n1 = Math.floor(Math.random() * 10)
+const n2 = Math.floor(Math.random() * 10)
+const n3 = Math.floor(Math.random() * 10)
+const n4 = Math.floor(Math.random() * 10)
+const n5 = Math.floor(Math.random() * 10)
+const n6 = Math.floor(Math.random() * 10)
+const numero = '' + n1 + n2 + n3 + n4 + n5 + n6
+console.log(numero)
