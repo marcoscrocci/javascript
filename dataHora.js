5b39dc13394dae5f309fd35da3c0972ae482d739
Marcos M. Crocci <marcoscrocci@gmail.com>
2020-05-17 00:45:08 -0300

    dataHora.js

---
+++ dataHora.js
@@ -0,0 +1,15 @@
+const moment = require('moment')
+
+let dataHoraAtual = moment('2020-05-16 06:00:00').toDate()
+let visualizou_em = moment('2020-05-16 00:00:00').toDate()
+
+console.log(moment(dataHoraAtual).format('DD/MM/YYYY HH:mm:ss'))
+
+dataValidade = moment(visualizou_em).add(6,'hours').toDate()
+
+if (dataHoraAtual >= dataValidade) {
+    console.log('Adicionar nova visualização')
+} else {
+    console.log('Já visualizou a menos de 6 horas')
+}
+
