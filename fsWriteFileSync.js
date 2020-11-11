5d2bc7b9f6c157e15ee39eab45f26cea00475905
Marcos M. Crocci <marcoscrocci@gmail.com>
2020-11-10 22:39:49 -0300

    Criar diretório

--- fsWriteFileSync.js
+++ fsWriteFileSync.js
@@ -1,5 +1,13 @@
 var fs = require("fs");
+const path = require("path");
+
+const dir = path.join(__dirname, 'arquivos/temp');
+if (!fs.existsSync(dir)) {
+	fs.mkdirSync(dir, {
+		recursive: true
+	});
+}
 
 var data = "Arquivo Texto gerado pelo Nodejs\nQuebra de linha";
 
-fs.writeFileSync("temp.txt", data)
+fs.writeFileSync(dir + "/temp.txt", data)
